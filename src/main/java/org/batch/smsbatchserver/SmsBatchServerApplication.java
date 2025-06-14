1315a7bb7f1e5f9b51dc4a92857d6af0943184de
jun <xodlf100@gmail.com>
2025-06-14 21:50:11 +0900

    first commit

---
+++ src/main/java/org/batch/smsbatchserver/SmsBatchServerApplication.java
@@ -0,0 +1,13 @@
+package org.batch.smsbatchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsBatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsBatchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/batch/smsbatchserver/SmsBatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.batch.smsbatchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsBatchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
