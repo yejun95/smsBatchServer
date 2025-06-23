62eda58ab7352a72d821ead352828a58095983b0
jun <xodlf100@gmail.com>
2025-06-24 00:41:13 +0900

    feat #2 SMS 전송 실패 시에도 로그 저장

--- src/main/java/org/batch/smsbatchserver/util/SmsSendUtil.java
+++ src/main/java/org/batch/smsbatchserver/util/SmsSendUtil.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Component;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -63,23 +64,43 @@ public class SmsSendUtil {
         message.setTo("01042139191");
         message.setText(num + "번째 테스트 sms api 랜덤값: " + numberKey);
 
-        SingleMessageSentResponse r = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        SingleMessageSentResponse r = null;
+        try {
+            r = this.messageService.sendOne(new SingleMessageSendingRequest(message));
 
-        SmsLog smsLog = SmsLog.builder()
-                .groupId(r.getGroupId())
-                .fromSMS(r.getFrom())
-                .toSMS(r.getTo())
-                .type(r.getType())
-                .statusMessage(r.getStatusMessage())
-                .country(r.getCountry())
-                .messageId(r.getMessageId())
-                .statusCode(r.getStatusCode())
-                .regDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                .build();
+            SmsLog smsLog = SmsLog.builder()
+                    .groupId(r.getGroupId())
+                    .fromSMS(r.getFrom())
+                    .toSMS(r.getTo())
+                    .type(r.getType())
+                    .statusMessage(r.getStatusMessage())
+                    .country(r.getCountry())
+                    .messageId(r.getMessageId())
+                    .statusCode(r.getStatusCode())
+                    .regDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                    .build();
 
-        smsRepository.save(smsLog);
-        log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "-> " + smsLog.getFromSMS() + "에서 " + smsLog.getToSMS() + "로 " + num + "번째 전송 완료");
-        return ResponseEntity.ok(num + "번째 전송 완료!!");
+            smsRepository.save(smsLog);
+            log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "-> " + smsLog.getFromSMS() + "에서 " + smsLog.getToSMS() + "로 " + num + "번째 전송 완료");
+            return ResponseEntity.ok(num + "번째 전송 완료!!");
+        } catch(Exception e) {
+            log.info(String.valueOf(e));
+            UUID uuid = UUID.randomUUID();
+            SmsLog smsLog = SmsLog.builder()
+                    .groupId(String.valueOf(uuid))
+                    .fromSMS(message.getFrom())
+                    .toSMS(message.getTo())
+                    .type(r != null ? r.getType() : null)
+                    .statusMessage(r != null ? r.getStatusMessage() : e.getMessage())
+                    .country(r != null ? r.getCountry() : null)
+                    .messageId(r != null ? r.getMessageId() : null)
+                    .statusCode(r != null ? r.getStatusCode() : "500")
+                    .regDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                    .build();
+
+            smsRepository.save(smsLog);
+            return ResponseEntity.internalServerError().body(num + "번째 전송 실패");
+        }
     }
 
 
